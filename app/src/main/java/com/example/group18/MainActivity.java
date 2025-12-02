82f70872c7baed6492c9688eda3d447858b4b51a
uwinezaflorence20 <uwinezaflorence20@gmail.com>
2025-12-02 15:04:03 +0200

    done with the design

--- app/src/main/java/com/example/group18/MainActivity.java
+++ app/src/main/java/com/example/group18/MainActivity.java
@@ -1,23 +1,20 @@
 package com.example.group18; // Make sure this package name matches yours
 
-import android.graphics.Color;
 import android.os.Bundle;
-import android.text.SpannableString;
-import android.text.style.ForegroundColorSpan;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-
-import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // This correctly sets the layout file.
         setContentView(R.layout.activity_main);
 
-        // --- START: Code to set up Toolbar and Tabs ---
+        // --- All the code that modified the tabs has been removed. ---
+        // --- We will now only set up the toolbar. ---
 
         // Set up the toolbar
         Toolbar toolbar = findViewById(R.id.toolbar);
@@ -26,28 +23,9 @@ public class MainActivity extends AppCompatActivity {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
             getSupportActionBar().setDisplayShowTitleEnabled(false); // Hide the default title
         }
-
-        // Find the TabLayout from the layout
-        TabLayout tabLayout = findViewById(R.id.tabLayout);
-
-        // Clear any tabs defined in XML to avoid duplicates
-        tabLayout.removeAllTabs();
-
-        // Add the "Events" tab
-        tabLayout.addTab(tabLayout.newTab().setText("Events"));
-
-        // --- Create and add the styled "Calendar 2" tab ---
-        SpannableString spannableString = new SpannableString("Calendar 2");
-        spannableString.setSpan(new ForegroundColorSpan(Color.RED), 9, 10, SpannableString.SPAN_INCLUSIVE_INCLUSIVE);
-        tabLayout.addTab(tabLayout.newTab().setText(spannableString));
-
-        // Add the "Hosting" tab
-        tabLayout.addTab(tabLayout.newTab().setText("Hosting"));
-
-        // --- END: Code to set up Toolbar and Tabs ---
     }
 
-    // This handles the click on the back arrow in the toolbar
+    // This method handles the click on the back arrow in the toolbar.
     @Override
     public boolean onSupportNavigateUp() {
         onBackPressed();
